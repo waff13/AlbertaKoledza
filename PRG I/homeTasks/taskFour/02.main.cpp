7e50c89e3610edb5a2d902b25a0e1c99d3897003
waff13 <mojmail@inbox.lv>
2021-12-14 22:17:31 +0200

    Update 02.main.cpp

--- PRG I/homeTasks/taskFour/02.main.cpp
+++ PRG I/homeTasks/taskFour/02.main.cpp
@@ -1,6 +1,155 @@
 #include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+string checkMove(char userInput, string openWord, string dottedWord);
+
+
+string getRandomWord() {
+	string words[] = {"alpha", "bravo", "charlie", "delta", "echo"};
+	int selected = (rand() % 5);
+	cout << "selected is " << selected << " and the word is "<<  words[selected] << endl;
+	
+	//return words[selected];
+	return "zaazzzaaertaazjhf";
+}
+
+string checkMove(char userInput, string openWord, string dottedWord) {
+	int result[20];
+	int counter = 0;
+	int length = openWord.length();
+	for (int i = 0, j = 0; i  < length; i++) {
+		if (openWord[i] == userInput) {
+			result[j] = i;
+			counter++;
+			j++;
+		}
+	}
+	
+	if (counter == 0) {
+		cout << "No such letter! -1 point" << endl; 
+	} else {
+		string foo;
+		foo.replace(0, 1, 1, userInput);
+		for (int i = 0; i < counter; i++){
+			cout << "Position of letter " << userInput << ": " << result[i] << endl;
+			dottedWord.replace(result[i], 1, foo);
+			//output.replace(3, 1, "z");    <- works, note that z is in "" ("z"; a string).
+		}
+	}
+	//cout << "Output line: " << dottedWord << endl;
+	
+	return dottedWord;
+}
+
+int main() {
+	
+	cout << "The game is on! Make your guess or press 0 to exit" << endl;
+	string openWord = getRandomWord();
+	string dottedWord;
+	for (int i = 0; i < openWord.length(); i++) {
+		dottedWord.append(".");
+	}
+	
+	cout << "  openWord: " << openWord << endl;
+	cout << "dottedWord: " << dottedWord << endl;
+	
+	char userInput;
+	while(userInput != 48)
+	{
+		userInput = getch();
+		cout << userInput << ": " << (int)userInput << endl;
+		dottedWord = checkMove(userInput, openWord, dottedWord);
+		cout << "Now -> " << dottedWord << endl;
+	}
+	
+	
+	/*string rwar = "qwerwererer";
+    //string rwar = "eeeee"; // used for test
+    char userZInput = 'e';
+    //string result = "";
+    int result[20];
+    int counter = 0;
+    int length = rwar.length();
+    cout << "Length is " << length << endl;
+    for (int i = 0, j = 0; i < length; i++) {
+        if (rwar[i]==userZInput) {
+            //result = result + to_string(i) + ",";
+            result[j] = i;
+            counter++;
+            j++;
+        }
+    }
+    //cout << "Result string: " << result << endl;
+    
+    string output = "..........."; // construct this string using the size of an initial word
+    //string output = "....."; // used for test
+    if (counter == 0) {
+        cout << "No such letter! -1 point" << endl;
+    } else {
+        string foooo;
+        foooo.replace(0, 1, 1, userZInput);
+        for (int i = 0; i < counter; i++){
+            cout << "Position of letter " << userZInput << ": " << result[i] << endl;
+            output.replace(result[i], 1, foooo);
+            //output.replace(3, 1, "z");    <- works, note that z is in "" ("z"; a string).
+        }
+    }
+    cout << "Output line: " << output << endl;*/
+	
+	
+	
+	
+    return 0;
+}
+
+
+
+
+/*#include <iostream>
 #include <string>
-#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    string words[3] = {"one", "two", "three"};
+	
+	for (int i = 0; i < 3; i++) {
+		cout << words[i] << endl;
+	}
+	
+	cout << "First letters: " << words[0][0] << words[1][0] << words[2][0] << endl;
+	
+	// pa vienai burtai - izmantojam getch()
+	
+	string s1;
+	
+	cout << "Input line: " << endl;
+	getline(cin, s1);
+	
+	while (s1.find("  ") != string::npos) {
+		s1.replace(s1.find("  "), 2, " ");
+	}
+	
+	if (s1[0] == ' ') {
+		s1.replace(0, 1, "");
+	}
+	
+	if (s1[s1.length()-1] == ' ') {
+		s1.replace(s1.length(), 1, "");
+	}
+	
+	cout << s1 << endl;
+	
+    return 0;
+}
+*/
+
+/*#include <iostream>
+#include <string>
+#include <conio.h>
 
 using namespace std;
 
@@ -71,18 +220,18 @@ int main()
     }
     cout << endl;
     
-    /*char userInput;
+    /*char userzInput;
     string s10 = "........"
-    while (userInput != 0) {
-        userInput = getch();
+    while (userzInput != 0) {
+        userzInput = getch();
         
-        while (hidden.find(userInput) != string::npos) {
-		s10.replace(hidden.find(userInput), 1, '.');
+        while (hidden.find(userzInput) != string::npos) {
+		s10.replace(hidden.find(userzInput), 1, '.');
 	}
         
     }*/
     
-    string str = "javatpoint";  
+    /*string str = "javatpoint";  
     int a = str.find('a');
     cout << "a = " << a << endl;
     
@@ -134,7 +283,7 @@ int main()
     cout << endl;
     cout << "04: "<< getRandomWord() << endl;
     cout << endl;
-    cout << "05: "<< getRandomWord() << endl;
+    cout << "05: "<< getRandomWord() << endl;*/
     
 	/*char c;
 	while(c!=48)
@@ -143,5 +292,5 @@ int main()
 		cout << c << ": " << (int)c << endl;
 	}*/
 	
-	return 0;
-}
+	/*return 0;
+}*/
