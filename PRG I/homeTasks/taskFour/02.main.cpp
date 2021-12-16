023eacec2e0e8d1fb3d147eb016e3dd00e767fe3
waff13 <mojmail@inbox.lv>
2021-12-16 15:12:24 +0200

    Update 02.main.cpp

--- PRG I/homeTasks/taskFour/02.main.cpp
+++ PRG I/homeTasks/taskFour/02.main.cpp
@@ -1,296 +1,130 @@
 #include <iostream>
-#include <conio.h>
+#include <string>
+#include <stdlib.h>
 
 using namespace std;
 
+int POINTSCOUNTER = 0; // counter for user points
+int WORDSGUESSED = 0; // number of words user have guessed
+bool GAMEOVER = false; // boolean to check if the game is over
+int RANGE = 20; // number of words is the game dictionary
+
+string dictionary[] = {"alabama", "arkansas", "colorado", "delaware", "georgia", "illinois", "kansas", "louisiana", "maryland", "michigan",
+			"montana", "nebraska", "oklahoma", "oregon", "texas", "utah", "vermont", "washington", "virginia", "vyoming"};
+
 string checkMove(char userInput, string openWord, string dottedWord);
 
-
 string getRandomWord() {
-	string words[] = {"alpha", "bravo", "charlie", "delta", "echo"};
-	int selected = (rand() % 5);
-	cout << "selected is " << selected << " and the word is "<<  words[selected] << endl;
-	
-	//return words[selected];
-	return "zaazzzaaertaazjhf";
+    
+    int selected = (rand() % (RANGE)); // getting random number
+    string result = dictionary[selected]; // getting the word from dictionary with that random number
+    string temp = dictionary[selected]; // and then moving this word to the end of the array
+    dictionary[selected] = dictionary[RANGE - 1];
+    dictionary[RANGE - 1] = temp;
+    RANGE--; // and shortening the array so the used words are in the end of array and not used while selecting new random words
+    return result;
+    
+    /* THIS CODE ALSO WORKS, JUST REMOVE 8//8 SIGNS
+    //string dictionary[] = {"alpha", "bravo", "charlie", "delta", "echo"};
+    //srand(time(0));
+    int selected = (rand() % (RANGE));
+    while (dictionary[selected] == "$$$") {
+        cout << "Inside while" << endl;
+        selected = (rand() % (RANGE));
+    }
+    string result = dictionary[selected];
+    
+    dictionary[selected] = "$$$";
+    
+    return result;*/
+}
+
+int calculatePoints() {
+	cout << "Points at start: " << POINTSCOUNTER << endl;
+	POINTSCOUNTER = POINTSCOUNTER - 5; // discarding 5 points from penalty score
+	if (POINTSCOUNTER < 0) { // if number of points is less than 0 we use 0 as this number cannot be negative
+		POINTSCOUNTER = 0;
+	}
+	cout << "Points after calculations: " << POINTSCOUNTER << endl;
+	return POINTSCOUNTER;
 }
 
 string checkMove(char userInput, string openWord, string dottedWord) {
-	int result[20];
-	int counter = 0;
-	int length = openWord.length();
+	int result[20]; // here we calculate how many of letters in the word the user have guessed with this try.
+	int counter = 0; // at the same time we calculate how many times the letter occurs in the word and record
+	int length = openWord.length(); // positions of these occurences. 
 	for (int i = 0, j = 0; i  < length; i++) {
-		if (openWord[i] == userInput) {
-			result[j] = i;
-			counter++;
+		if (openWord[i] == userInput) { // we go letter by letter in the word and if user input matches a letter in the word
+			result[j] = i; // we record it 
+			counter++; // and increase the counter by one
 			j++;
 		}
 	}
 	
-	if (counter == 0) {
-		cout << "No such letter! -1 point" << endl; 
-	} else {
-		string foo;
-		foo.replace(0, 1, 1, userInput);
+	if (counter == 0) { // if counter = 0 this means user guess is not correct
+		cout << "No such letter! -1 point" << endl;
+		POINTSCOUNTER++; // so we are giving him 1 point
+		if (POINTSCOUNTER == 10) { // if the amount of points equals 10 the game is over, 
+			cout << "Sorry, you have lost!" << endl;
+			cout << "You have guessed " << WORDSGUESSED << "of 20 words." << endl; // and we print the final result
+			GAMEOVER = true;
+			return 0;
+		}
+	} else { // but if the user input matches at least one letter in the word
+		string line;
+		line.replace(0, 1, 1, userInput); // we create a 1 symbol long string (for some reason an easy option of "string line(1, userInput);" failed for me)
 		for (int i = 0; i < counter; i++){
-			cout << "Position of letter " << userInput << ": " << result[i] << endl;
-			dottedWord.replace(result[i], 1, foo);
+			//cout << "Position of letter " << userInput << ": " << result[i] << endl;
+			dottedWord.replace(result[i], 1, line); // and put in instead of dots in the correct places
 			//output.replace(3, 1, "z");    <- works, note that z is in "" ("z"; a string).
 		}
 	}
 	//cout << "Output line: " << dottedWord << endl;
+	
+	if (dottedWord.find(".") == string::npos) { // if the are no more . in the word that means the user have guessed the word
+		if (WORDSGUESSED == 19) { // if guessed words counter reaches 19 this means all the words are guessed and user has won
+			cout << "Congratulations, you have won this game!" << endl;
+			GAMEOVER = true;
+			return 0;
+		} else { // if not, that means we need to calculate user points and continue with the game
+			cout << "OK, you have guessed this word!" << endl;
+			calculatePoints();
+			WORDSGUESSED++; // we increase the amount of guessed words by 1
+			cout << "That was word " << WORDSGUESSED << " of 20!" << endl;
+		}
+	}
 	
 	return dottedWord;
 }
 
 int main() {
 	
-	cout << "The game is on! Make your guess or press 0 to exit" << endl;
-	string openWord = getRandomWord();
-	string dottedWord;
-	for (int i = 0; i < openWord.length(); i++) {
-		dottedWord.append(".");
+	while(!GAMEOVER){ // the game runs while it is not over
+		
+		cout << "The game is on! Make your guess or press 0 to exit" << endl;
+		string openWord = getRandomWord(); // getting the random word from array
+		string dottedWord;
+		for (int i = 0; i < openWord.length(); i++) { // creating the ... representation of the random word
+			dottedWord.append(".");
+		}
+		
+		cout << "  openWord: " << openWord << endl;
+		cout << "dottedWord: " << dottedWord << endl;
+		
+		char userInput;
+		//while(userInput != 48)
+		while (dottedWord.find(".") != string::npos) { // while user has not guessed the word (== while there are . in this word)
+			userInput = getch(); // we read his input
+			if (userInput == 48) { // if user inputs 0 the game is over
+				cout << "OK, see you, bye!" << endl;
+				GAMEOVER = true; 
+				return 0;
+			}
+			//cout << userInput << ": " << (int)userInput << endl;
+			dottedWord = checkMove(userInput, openWord, dottedWord); // checking if the user has guessed the letter and word 
+			cout << "Now -> " << dottedWord << endl;
+		}
 	}
-	
-	cout << "  openWord: " << openWord << endl;
-	cout << "dottedWord: " << dottedWord << endl;
-	
-	char userInput;
-	while(userInput != 48)
-	{
-		userInput = getch();
-		cout << userInput << ": " << (int)userInput << endl;
-		dottedWord = checkMove(userInput, openWord, dottedWord);
-		cout << "Now -> " << dottedWord << endl;
-	}
-	
-	
-	/*string rwar = "qwerwererer";
-    //string rwar = "eeeee"; // used for test
-    char userZInput = 'e';
-    //string result = "";
-    int result[20];
-    int counter = 0;
-    int length = rwar.length();
-    cout << "Length is " << length << endl;
-    for (int i = 0, j = 0; i < length; i++) {
-        if (rwar[i]==userZInput) {
-            //result = result + to_string(i) + ",";
-            result[j] = i;
-            counter++;
-            j++;
-        }
-    }
-    //cout << "Result string: " << result << endl;
-    
-    string output = "..........."; // construct this string using the size of an initial word
-    //string output = "....."; // used for test
-    if (counter == 0) {
-        cout << "No such letter! -1 point" << endl;
-    } else {
-        string foooo;
-        foooo.replace(0, 1, 1, userZInput);
-        for (int i = 0; i < counter; i++){
-            cout << "Position of letter " << userZInput << ": " << result[i] << endl;
-            output.replace(result[i], 1, foooo);
-            //output.replace(3, 1, "z");    <- works, note that z is in "" ("z"; a string).
-        }
-    }
-    cout << "Output line: " << output << endl;*/
-	
-	
-	
-	
-    return 0;
+
+return 0;
 }
-
-
-
-
-/*#include <iostream>
-#include <string>
-
-using namespace std;
-
-int main()
-{
-    string words[3] = {"one", "two", "three"};
-	
-	for (int i = 0; i < 3; i++) {
-		cout << words[i] << endl;
-	}
-	
-	cout << "First letters: " << words[0][0] << words[1][0] << words[2][0] << endl;
-	
-	// pa vienai burtai - izmantojam getch()
-	
-	string s1;
-	
-	cout << "Input line: " << endl;
-	getline(cin, s1);
-	
-	while (s1.find("  ") != string::npos) {
-		s1.replace(s1.find("  "), 2, " ");
-	}
-	
-	if (s1[0] == ' ') {
-		s1.replace(0, 1, "");
-	}
-	
-	if (s1[s1.length()-1] == ' ') {
-		s1.replace(s1.length(), 1, "");
-	}
-	
-	cout << s1 << endl;
-	
-    return 0;
-}
-*/
-
-/*#include <iostream>
-#include <string>
-#include <conio.h>
-
-using namespace std;
-
-int RANGE = 5;
-int lastOne = 4;
-
-string getRandomWord() {
-    
-    string dictionary[] = {"alpha", "bravo", "charlie", "delta", "echo"};
-    int selected = (rand() % (RANGE + 1));
-    cout << "Range = " << RANGE << " and selected = " << selected << endl;
-    string result = dictionary[selected];
-    string temp = dictionary[selected];
-    dictionary[selected] = dictionary[lastOne];
-    dictionary[lastOne] = temp;
-
-
-    
-    //cout << "Return result is " << result << endl;
-    cout << "Array after element move:" << endl;
-    //cout << "Now range = " << RANGE << " and selected = " << selected << endl;
-    for (int i = 0; i < 5; i++) {
-        cout << dictionary[i] + ", ";
-    }
-    cout << endl;
-    RANGE --;
-    lastOne--;
-    
-    return result;
-}
-
-int main()
-{
-	string words[3] = {"one", "two", "three"};
-	
-	for (int i = 0; i < 3; i++) {
-		cout << words[i] << endl;
-	}
-	
-	cout << "First letters: " << words[0][0] << words[1][0] << words[2][0] << endl;
-	
-	// pa vienai burtai - izmantojam getch()
-	
-	string s1;
-	
-	cout << "Input line: " << endl;
-	getline(cin, s1);
-	
-	while (s1.find("  ") != string::npos) {
-		s1.replace(s1.find("  "), 2, " ");
-	}
-	
-	if (s1[0] == ' ') {
-		s1.replace(0, 1, "");
-	}
-	
-	if (s1[s1.length()-1] == ' ') {
-		s1.replace(s1.length(), 1, "");
-	}
-	
-	cout << s1 << endl;
-	
-	string hidden = "barbeque";
-    cout << hidden << endl;
-    int size = hidden.length();
-    for (int i = 0; i < size; i++) {
-        cout << '.';
-    }
-    cout << endl;
-    
-    /*char userzInput;
-    string s10 = "........"
-    while (userzInput != 0) {
-        userzInput = getch();
-        
-        while (hidden.find(userzInput) != string::npos) {
-		s10.replace(hidden.find(userzInput), 1, '.');
-	}
-        
-    }*/
-    
-    /*string str = "javatpoint";  
-    int a = str.find('a');
-    cout << "a = " << a << endl;
-    
-    string rwar = "qwerwererer";
-    //string rwar = "eeeee"; // used for test
-    char userZInput = 'e';
-    //string result = "";
-    int result[20];
-    int counter = 0;
-    int length = rwar.length();
-    cout << "Length is " << length << endl;
-    for (int i = 0, j = 0; i < length; i++) {
-        if (rwar[i]==userZInput) {
-            //result = result + to_string(i) + ",";
-            result[j] = i;
-            counter++;
-            j++;
-        }
-    }
-    //cout << "Result string: " << result << endl;
-    
-    string output = "..........."; // construct this string using the size of an initial word
-    //string output = "....."; // used for test
-    if (counter == 0) {
-        cout << "No such letter! -1 point" << endl;
-    } else {
-        string foooo;
-        foooo.replace(0, 1, 1, userZInput);
-        for (int i = 0; i < counter; i++){
-            cout << "Position of letter " << userZInput << ": " << result[i] << endl;
-            output.replace(result[i], 1, foooo);
-            //output.replace(3, 1, "z");    <- works, note that z is in "" ("z"; a string).
-        }
-    }
-    cout << "Output line: " << output << endl;
-    
-    // check if the word is guessed
-    if (output.find(".") != string::npos){
-        cout << "There are some . left" << endl;
-    } else {
-        cout << "The word is guessed entirely!" << endl;
-    }
-    
-    cout << "01: "<< getRandomWord() << endl;
-    cout << endl;
-    cout << "02: "<< getRandomWord() << endl;
-    cout << endl;
-    cout << "03: "<< getRandomWord() << endl;
-    cout << endl;
-    cout << "04: "<< getRandomWord() << endl;
-    cout << endl;
-    cout << "05: "<< getRandomWord() << endl;*/
-    
-	/*char c;
-	while(c!=48)
-	{
-		c=getch();
-		cout << c << ": " << (int)c << endl;
-	}*/
-	
-	/*return 0;
-}*/
