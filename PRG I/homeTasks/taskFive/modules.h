6862a878e5b3cb7e472d059cf1dd21c40c42b164
waff13 <mojmail@inbox.lv>
2022-01-06 21:32:01 +0200

    Create modules.h

---
+++ PRG I/homeTasks/taskFive/modules.h
@@ -0,0 +1,13 @@
+#ifndef MODULES_H
+#define MODULES_H
+
+void addRecord();
+void changeBalance();
+void copyDB();
+void countRecords ();
+void deleteRecord();
+void displayAll();
+void displayNegative();
+void findRecord();
+
+#endif
